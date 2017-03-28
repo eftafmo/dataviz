005556592c3f39e689934da9305c773203bf6d68
Ionuț Ciocîrlan <ionut.ciocirlan@eaudeweb.ro>
2017-03-28 16:46:29 +0300

    move templates around

--- dv/views/frontend.py
+++ dv/views/frontend.py
@@ -3,5 +3,5 @@ from django.template.loader import render_to_string
 
 
 def home(request):
-    html = render_to_string('home.html')
+    html = render_to_string('homepage.html')
     return HttpResponse(html)
