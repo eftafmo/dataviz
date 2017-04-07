d98dbc270ef68adc607d762249d7bb457c19ad4c
Valentin Popescu <ppsc.valentin@gmail.com>
2017-04-07 17:04:07 +0300

    responsive header menu

---
+++ assets/js/header.js
@@ -0,0 +1,73 @@
+window.Header = (function() {
+
+  var el = document.querySelector('.header');
+
+  var backdrop = function() {
+    var el = document.createElement('DIV');
+    el.id = 'backdrop';
+    document.body.appendChild(el);
+
+    return {
+      el: el,
+      show: function() { el.style.display = 'block'; },
+      hide: function() { el.style.display = 'none'; }
+    }
+  }();
+
+  var menus = {
+    'search': el.querySelector('.header-search'),
+    'menu': el.querySelector('.header-menu')
+  };
+
+  var active;
+
+  function _bodyClass(targetName) {
+    return targetName + '-is-open';
+  }
+
+  var open = function(targetName) {
+    var target = menus[targetName];
+
+    close();
+
+    active = targetName;
+    document.body.classList.add(_bodyClass(targetName));
+
+    target.classList.add('is-open');
+    backdrop.show();
+
+    if (targetName === 'search')
+      target.querySelector('input').focus();
+  };
+
+  var close = function() {
+    if (active) {
+      var target = menus[active];
+      
+      document.body.classList.remove(_bodyClass(active));
+      target.classList.remove('is-open');
+
+      active = undefined;
+      backdrop.hide();
+    }
+  };
+
+  backdrop.el.addEventListener('click', close, false);
+
+  window.addEventListener('keyup', function(e) {
+    if (e.keyCode === 27 && active)
+      close()
+  });
+
+  return {
+    open: open,
+    close: close,
+    toggle: function(targetName) {
+      if (active != targetName) {
+        open(targetName)
+      } else {
+        close()
+      }
+    }
+  }
+}());
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@
 require("../css/normalize.css");
 require("../css/main.css");
 
+require("../js/header.js");
+
 import * as d3 from "d3";
 import {sankey as d3_sankey} from "d3-sankey";
 import Vue from "vue";
