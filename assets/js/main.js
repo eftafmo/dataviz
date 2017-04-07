3609bb8d7cef93beebb62557350a4ca6ab3ea2f8
Valentin Popescu <ppsc.valentin@gmail.com>
2017-04-07 18:05:39 +0300

    [wip] raw sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,10 +11,10 @@ import Vue from "vue";
 
 import MainApp from './MainApp.vue';
 
-new Vue({
-  el: '#content',
-  render: h => h(MainApp)
-});
+// new Vue({
+//   el: '#content',
+//   render: h => h(MainApp)
+// });
 
 
 require("../css/sankey.css");
@@ -23,7 +23,7 @@ let margin = {top: 10, right: 10, bottom: 10, left: 10},
     width = 700 - margin.left - margin.right,
     height = 300 - margin.top - margin.bottom;
 
-let svg = d3.select("#content").append("svg")
+let svg = d3.select("#content")//.append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
