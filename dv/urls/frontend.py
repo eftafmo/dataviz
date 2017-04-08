41d61f84e098ebd4bdaab03b8bd8ffe5ea60c8d8
Valentin Popescu <ppsc.valentin@gmail.com>
2017-04-08 16:51:45 +0300

    [dev-only] added view to play around

--- dv/urls/frontend.py
+++ dv/urls/frontend.py
@@ -4,4 +4,8 @@ from dv.views import frontend as views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+
+    # [dev-only] sandbox for testing ui components
+    url(r'^sandbox/$', views.sandbox, name='sandbox'),
 ]
+
--- dv/views/frontend.py
+++ dv/views/frontend.py
@@ -5,3 +5,7 @@ from django.template.loader import render_to_string
 def home(request):
     html = render_to_string('homepage.html')
     return HttpResponse(html)
+
+def sandbox(request):
+    html = render_to_string('sandbox.html')
+    return HttpResponse(html)
