942162d8852ed57a1ca3c807dcc9f232b4e10915
mihai-macaneata <themacaneata@gmail.com>
2017-05-02 00:11:59 +0300

    reset filters button styling for global filters bar

--- assets/js/components/globalfilter.vue
+++ assets/js/components/globalfilter.vue
@@ -1,26 +1,42 @@
 <template>
-    <div class="global-filter">
-        <span class="filter-item"
-         v-for="(item, type) in items"
-         :filter="type"
-         @click="removeFilter()">
-        {{item}}
-        </span>
+    <div class="global-filters">
+      <div v-if="checkFilter()" class="container">
+        <div class="global-filters-inner">
+          <div class="filters-label">
+            Showing data for:
+          </div>
+            <ul class="list-filters">
+              <li class="filter-item"
+               v-for="(item, type, value) in items"
+               :filter="type"
+               v-if="item != null"
+               @click="removeFilter()">
+                {{item}}
+                <span class="icon icon-close"></span>
+              </li>
+            </ul>
+            <button @click="resetFilters()" class="no-btn muted" id="reset-filters">
+              Reset filters <span class="icon icon-close"></span>
+            </button>
+        </div>
+      </div>
     </div>
 </template>
 
 
 <style lang="less">
 
-.global-filter {
-  background: #50B9FF;
-  color: white;
-}
 .filter-item {
-  // padding: .5rem;
-  margin-right: 1rem;
+  cursor: pointer;
   margin-left: .5rem;
-  // display: inline;
+}
+
+.global-filters {
+  padding: 0;
+
+  .container {
+    padding: .5em 3rem;
+  }
 }
 
 </style>
@@ -31,13 +47,7 @@ import Vue from 'vue';
 import * as d3 from 'd3';
 import {FILTERS} from './global/filters.js'
 
-
-
 export default Vue.extend({
-  props : {
-    item: String,
-    filter: String,
-  },
 
   data: function () {
     return {
@@ -45,11 +55,27 @@ export default Vue.extend({
      }
    },
 
-
   methods: {
     removeFilter(){
       var remove_el = event.target.getAttribute('filter')
       FILTERS[remove_el] = null;
+    },
+
+    resetFilters(){
+      Object.keys(FILTERS).forEach(function(key) {
+         FILTERS[key]=null
+      });
+    },
+
+    checkFilter(){
+      var return_val;
+      Object.keys(FILTERS).forEach(function(key) {
+          if(FILTERS[key]!=null){
+            console.log(FILTERS[key]);
+            return_val = true;
+          }
+      });
+      return return_val
     }
   },
 
