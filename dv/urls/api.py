61c04ba948cd63e08fc6f7e88c229b44dab1a208
danbaragan <daniel.mihai.baragan@gmail.com>
2017-05-15 19:35:52 +0300

    Add api/grants.json
    Fetches most of the grants page data in a flat structure.
    Including funds for bilateral relations from ProgrammeOutcome
    Does this in a fast way through careful use of prefetch_related
    
    Results, Programmes, NUTS are to be fetched separately.

--- dv/urls/api.py
+++ dv/urls/api.py
@@ -3,10 +3,13 @@ from dv.views import api as views
 
 
 urlpatterns = [
-    url(r'^allocation.csv', views.beneficiaries_fm_gross_allocation,
-        name='beneficiary-fm-allocation'),
-    url(r'^sectors.json', views.sectors_areas_allocation,
-        name='sector-allocation'),
-    url(r'^beneficiaries.csv', views.beneficiary_allocation,
-        name='beneficiary-allocation'),
+    # TODO Add this back if you want to test api/grants.json without changing the UI.
+    # TODO Remove them after the change
+    # url(r'^allocation.csv', views.beneficiaries_fm_gross_allocation,
+    #     name='beneficiary-fm-allocation'),
+    # url(r'^sectors.json', views.sectors_areas_allocation,
+    #     name='sector-allocation'),
+    # url(r'^beneficiaries.csv', views.beneficiary_allocation,
+    #     name='beneficiary-allocation'),
+    url(r'^grants.json', views.grants, name='grants'),
 ]
--- dv/views/api.py
+++ dv/views/api.py
@@ -1,7 +1,44 @@
 from collections import OrderedDict, defaultdict, namedtuple
+
+from django.http import HttpResponseNotAllowed
 from django.utils.text import slugify
 from dv.lib.http import CsvResponse, JsonResponse
-from dv.models import State, ProgrammeArea
+from dv.models import State, ProgrammeArea, Allocation
+
+
+def grants(request):
+    if request.method != 'GET':
+        return HttpResponseNotAllowed()
+
+    allocations = Allocation.objects.all().select_related(
+        'state',
+        'programme_area',
+        'programme_area__priority_sector',
+    ).prefetch_related(
+        # TODO figure out how to use Prefetch() here to filter outcome.name from the begining
+        'programme_area__outcomes__programmes',
+    )
+    out = []
+    for a in allocations:
+        out.append({
+            'BSCode': a.state.code,
+            'BSName': a.state.name,
+            'FMCode': a.financial_mechanism_id,
+            'PACode': a.programme_area.code,
+            'PAShort': a.programme_area.short_name,
+            'PAName': a.programme_area.name,
+            'PSCode': a.programme_area.priority_sector.code,
+            'PSName': a.programme_area.priority_sector.name,
+            'GrossAlloc': a.gross_allocation,
+            'NetAlloc': a.net_allocation,
+            # Make sure you select the exact querySet from related manager as we did on prefetch_related
+            # That is all(). This is why we filter the outcome.name in python.
+            # Otherwise the prefetch is useless and this will take a *lot* of time
+            'BilateralAlloc': sum(p.allocation
+                for o in a.programme_area.outcomes.all() if o.name == 'Fund for bilateral relations'
+                    for p in o.programmes.all()),
+        })
+    return JsonResponse(out)
 
 
 def beneficiaries_fm_gross_allocation(request):
