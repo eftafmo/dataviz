7e1cb3d33721f9d20db89232e6da61b7f14b2620
danbaragan <daniel.mihai.baragan@gmail.com>
2017-05-28 19:51:29 +0300

    Add first facets; give up dynamic fields and relly on ./manage.py build_solr_schema

--- dv/search_indexes.py
+++ dv/search_indexes.py
@@ -3,8 +3,13 @@ from dv.models import Programme
 
 
 class ProgrammeIndex(indexes.SearchIndex, indexes.Indexable):
-    name = indexes.CharField(index_fieldname='name_s', model_attr='name')
+    name = indexes.CharField(model_attr='name')
+    # TODO use template here to include more than summary
     text = indexes.CharField(document=True, model_attr='summary')
+
+    status = indexes.CharField(model_attr='status', faceted=True)
+    state_code = indexes.CharField(model_attr='state__code', faceted=True)
+    state_name = indexes.CharField(model_attr='state__name')
 
     def get_model(self):
         return Programme
