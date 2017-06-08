8cdecf727a534a3a18d522e596d247cbddcbcb5c
danbaragan <daniel.mihai.baragan@gmail.com>
2017-06-08 20:51:57 +0300

    Add index for programmes -- WIP

--- dv/search_indexes.py
+++ dv/search_indexes.py
@@ -11,6 +11,15 @@ class ProgrammeIndex(indexes.SearchIndex, indexes.Indexable):
     state_code = indexes.CharField(model_attr='state__code', faceted=True)
     state_name = indexes.CharField(model_attr='state__name')
 
+    programme_area_ss = indexes.FacetMultiValueField(model_attr='programme_areas__name')
+    priority_sector_ss = indexes.FacetMultiValueField(model_attr='programme_areas__priority_sector__name')
+
     def get_model(self):
         return Programme
+
+    def prepare_programme_area_ss(self, obj):
+        return list(set([ pa.name for pa in obj.programme_areas.all() ]))
+
+    def prepare_priority_sector_ss(self, obj):
+        return list(set([ pa.priority_sector.name for pa in obj.programme_areas.all() ]))
 
