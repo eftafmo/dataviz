71e4abb6eaf30de2b6e0193167145566be9f627c
Ionuț Ciocîrlan <ionut.ciocirlan@eaudeweb.ro>
2017-06-09 17:01:20 +0300

    root vue instance on homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,9 @@ import './widgets/index.js';
 
 // expose root vue instances globally
 // TODO: in a cleaner way?
+import Homepage from './Homepage.js';
 import Grants from './Grants.js';
+window.Homepage = Homepage;
 window.Grants = Grants;
 
 //keep homepage layout as a one column layout
