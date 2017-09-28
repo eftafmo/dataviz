3ee4022f98c0e185c2a19eb169a7cccb9ecbfe92
Andreea Dima <andreeadima95@gmail.com>
2017-09-28 13:53:05 +0300

    Remove old urls

--- dv/urls/api.py
+++ dv/urls/api.py
@@ -7,18 +7,6 @@ from dv.views import frontend as front_views
 
 
 urlpatterns = [
-    # TODO Add this back if you want to test api/grants.json without changing the UI.
-    # TODO Remove them after the change
-    # url(r'^allocation.csv',
-    #     views.beneficiaries_fm_gross_allocation,
-    #     name='beneficiary-fm-allocation'),
-    # url(r'^sectors.json',
-    #     views.sectors_areas_allocation,
-    #     name='sector-allocation'),
-    # url(r'^beneficiaries.csv',
-    #     views.beneficiary_allocation,
-    #     name='beneficiary-allocation'),
-
     url(r'^overview.json',
         cache_page(settings.API_CACHE_SECONDS)(views.overview),
         name='index'),
