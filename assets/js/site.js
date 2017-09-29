98c388e2d2d0d3753ba006a3326f6aaaa47e0754
Ionuț Ciocîrlan <ionut.ciocirlan@eaudeweb.ro>
2017-09-29 19:16:57 +0300

    split search-related js into separate module. closes #424

--- assets/js/site.js
+++ assets/js/site.js
@@ -11,3 +11,5 @@ import './widgets/index.js';
 
 //expose root vue instances globally
 require("expose-loader?root!./root-instances.js");
+//expose search stuff globally
+require("expose-loader?search!./search.js");
