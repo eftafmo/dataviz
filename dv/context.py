4c600baaa0b76d9878d60051543c1018d4c72277
Diana Boiangiu <boiangiudiana58@gmail.com>
2017-12-13 12:27:49 +0200

    Fixes #539

--- dv/context.py
+++ dv/context.py
@@ -2,9 +2,9 @@ from django.conf import settings
 
 
 def get_context(request):
-    expose_settings = [
-        'GOOGLE_ANALYTICS_PROPERTY_ID',
-    ]
+    expose_settings = []
+    if not settings.DEBUG:
+        expose_settings = ['GOOGLE_ANALYTICS_PROPERTY_ID']
 
     exposed_settings = {
         s: getattr(settings, s, None)
