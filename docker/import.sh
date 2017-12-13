7463611dc6c32a2b99c70a01d634dee43cd6031b
Andrei Melis <andrei.melis@eaudeweb.ro>
2017-12-13 09:10:52 +0200

    Don't reindex unless data imported, fixes #543

--- docker/import.sh
+++ docker/import.sh
@@ -2,6 +2,7 @@
 
 date '+%Y-%m-%d %H:%M:%S'
 $PYTHONPATH/python /var/local/dataviz/manage.py import_news
+$PYTHONPATH/python /var/local/dataviz/manage.py update_index dv.news
 
 UPLOAD_DIR=/var/local/upload
 date '+%Y-%m-%d %H:%M:%S'
@@ -10,8 +11,8 @@ if [ $? -eq 0 ]; then
 	DONE_DIR=`date '+%Y%m%d'`
 	mkdir $UPLOAD_DIR/$DONE_DIR
 	mv $UPLOAD_DIR/*.xlsx $UPLOAD_DIR/$DONE_DIR
+	date '+%Y-%m-%d %H:%M:%S'
+	$PYTHONPATH/python /var/local/dataviz/manage.py rebuild_index --noinput
+	date '+%Y-%m-%d %H:%M:%S'
 fi
 
-date '+%Y-%m-%d %H:%M:%S'
-$PYTHONPATH/python /var/local/dataviz/manage.py rebuild_index --noinput
-date '+%Y-%m-%d %H:%M:%S'
