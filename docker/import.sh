126a19e6c4cf52050a61ba3e00ea1bbd0d17aad5
--global <andrei.melis@eaudeweb.ro>
2018-06-09 14:37:36 +0200

    Fix typo.

--- docker/import.sh
+++ docker/import.sh
@@ -6,7 +6,7 @@ $PYTHONPATH/python /var/local/dataviz/manage.py update_index dv.news --remove
 
 UPLOAD_DIR=/var/local/upload
 date '+%Y-%m-%d %H:%M:%S'
-output=$($PYTHONPATH/python /var/local/dataviz/manage.py import $UPLOAD_DIR)
+output=$($PYTHONPATH/python /var/local/dataviz/manage.py import $UPLOAD_DIR 2>&1)
 if [ $? -eq 0 ]; then
 	DONE_DIR=`date '+%Y%m%d'`
 	mkdir $UPLOAD_DIR/$DONE_DIR
@@ -15,6 +15,6 @@ if [ $? -eq 0 ]; then
 	$PYTHONPATH/python /var/local/dataviz/manage.py rebuild_index --noinput
 	date '+%Y-%m-%d %H:%M:%S'
 else
-	sentry-cli send-event -m "Import error.\n$output"
+	sentry-cli send-event -m "Import error: $output"
 fi
 
