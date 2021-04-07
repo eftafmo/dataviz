92ebadfe3e3a23b277adb1446dc9d4aa899ecfa8
Alex Medesan <alex.medesan@gmail.com>
2021-04-07 16:32:59 +0300

    upgraded sentry library for vue to @sentry/vue

--- assets/dataviz.js
+++ assets/dataviz.js
@@ -4,27 +4,21 @@
 require("./css/dataviz.less")
 
 // register polyfills
-import './js/lib/polyfills'
+import * as Sentry from '@sentry/vue';
+import Vue from 'vue';
+import VueSuper from 'vue-super';
+import './js/lib/polyfills';
 
-// install vue global plugins
-import Vue from 'vue'
-import VueSuper from 'vue-super'
 Vue.use(VueSuper)
-
-
-// register sentry if production environment
-import Raven from 'raven-js';
-import RavenVue from 'raven-js/plugins/vue';
 
 if (process.env.NODE_ENV === "production") {
   console.info("Running in production mode! Enabling Sentry.");
 
-  Raven
-    .config(window._dv_sentry_config.dsn, {
-      environment: window._dv_sentry_config.environment
-    })
-    .addPlugin(RavenVue, Vue)
-    .install();
+  Sentry.init({
+    Vue: Vue,
+    dsn: window._dv_sentry_config.dsn,
+  })
+
 }
 else {
   console.info("Development mode, no sentry.");
