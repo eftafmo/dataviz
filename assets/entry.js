884604e2e8077f5f5d679213107dce4e395cbcfa
Ionuț Ciocîrlan <ionut.ciocirlan@gmail.com>
2021-05-06 19:33:40 +0300

    tab components

--- assets/entry.js
+++ assets/entry.js
@@ -1,5 +1,18 @@
 import * as Vue from 'vue/dist/vue.esm-bundler.js'
+import Tabs from 'vue3-tabs-component/src/components/Tabs.vue';
+import Tab from 'vue3-tabs-component/src/components/Tab.vue';
 import * as Dataviz from './js/root-instances.js'
 
-window._createApp = Vue.createApp
+
+function createApp(spec) {
+  const app = Vue.createApp(spec)
+
+  app
+    .component('tabs', Tabs)
+    .component('tab', Tab)
+
+  return app
+}
+
+window._createApp = createApp
 window.Dataviz = Dataviz
