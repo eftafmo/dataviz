4f24c14a7277363a7dae3c8cb305dd6f687a3436
Ionuț Ciocîrlan <ionut.ciocirlan@gmail.com>
2021-06-04 12:54:28 +0300

    "widgets" not needed anymore

--- assets/js/widgets/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-// !!! TODO !!!
-//import {Tabs, Tab} from 'vue-tabs-component';
-import Tag from './Tag';
-
-//Vue.component('tabs', Tabs);
-//Vue.component('tab', Tab);
-Vue.component('tag', Tag);
