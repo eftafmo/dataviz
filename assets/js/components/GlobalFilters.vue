33b18e36c65d53b974840cb3720a2424bf827b9e
Alexandru Chirila <alex@alexkiro.me>
2021-07-15 14:16:52 +0300

    Update global filters

--- assets/js/components/GlobalFilters.vue
+++ assets/js/components/GlobalFilters.vue
@@ -1,40 +1,41 @@
 <template>
-<div :class="{ on_top: hasFilters() }" id="global-filters">
-  <div v-if="hasFilters()" class="global-filters" :class="{active: hasFilters()}">
-    <transition name="bounce">
-      <div v-if="hasFilters" class="container">
-        <div class="global-filters-inner">
-          <div class="filters-label">
-            Showing data for:
-          </div>
-          <ul class="list-filters">
-           <transition-group name="list">
-              <li class="filter-item"
+  <div :class="{ on_top: hasFilters() }" id="global-filters">
+    <div v-if="hasFilters()" class="global-filters" :class="{active: hasFilters()}">
+      <transition name="bounce">
+        <div v-if="hasFilters" class="container">
+          <div class="global-filters-inner">
+            <div class="filters-label">
+              Showing data for:
+            </div>
+            <transition-group name="list">
+              <div
+                  class="filter-item"
                   v-for="(item, key) in Object.fromEntries(
-                    Object.entries(data).filter(v => v != null)
-                  )"
+                  Object.entries(data).filter(v => v != null)
+                )"
                   :key="key"
                   @click="removeFilter(key)"
               >
-               {{item.name}}: {{item.value}}
-              </li>
+                {{ item.name }}: {{ item.value }}
+                <span class="icon icon-cross"></span>
+              </div>
             </transition-group>
-          </ul>
-          <button @click="resetFilters" class="no-btn" id="reset-filters">
-            Reset filters <span class="icon icon-cross"></span>
-          </button>
+            <button @click="resetFilters" class="no-btn" id="reset-filters">
+              Reset filters <span class="icon icon-cross"></span>
+            </button>
+          </div>
         </div>
-      </div>
-    </transition>
-  </div>
+      </transition>
+    </div>
   </div>
 </template>
 
 
 <style lang="less">
-.on_top{
+.on_top {
   z-index: 2;
 }
+
 .filter-item {
   cursor: pointer;
   margin-left: .5rem;
@@ -43,13 +44,13 @@
 .list-filters {
   overflow: hidden;
   white-space: normal;
-  @media(max-width: 800px){
+  @media (max-width: 800px) {
     margin-left: -2rem;
   }
 }
 
 #reset-filters {
-  @media(max-width: 800px) {
+  @media (max-width: 800px) {
     margin-right: -2rem;
   }
 }
@@ -68,14 +69,16 @@
 
   .container {
     padding: .5rem 3rem;
+    text-align: right;
   }
 }
+
 .global-filters .icon {
   font-size: 1rem;
 }
 
 .filters-label {
-  @media(max-width:800px){
+  @media (max-width: 800px) {
     display: none;
   }
 }
@@ -83,22 +86,15 @@
 .filter-item {
   display: inline-block;
 
-  &:after {
-    content: "\ea0f";
-    font-family: 'icomoon' !important;
-    vertical-align: middle;
-    font-size: smaller;
-    -webkit-text-stroke: 2px #3D90F3;
+  @media (max-width: 800px) {
+    display: block;
   }
-
- @media(max-width: 800px){
-  display: block;
- }
 }
 
 .list-enter-active, .list-leave-active {
   transition: all .3s;
 }
+
 .list-enter, .list-leave-to {
   opacity: 0;
   transform: translateY(30px);
@@ -107,9 +103,11 @@
 .bounce-enter-active {
   animation: bounce-in .3s;
 }
+
 .bounce-leave-active {
   animation: bounce-out .3s;
 }
+
 @keyframes bounce-in {
   0% {
     transform: scale(0);
@@ -121,6 +119,7 @@
     transform: scale(1);
   }
 }
+
 @keyframes bounce-out {
   0% {
     transform: scale(1);
@@ -136,62 +135,61 @@
 </style>
 
 
-
 <script>
-import * as d3 from 'd3';
-import {truncate} from '@js/lib/util';
-import _programme_areas from '@js/constants/programme-areas.json5';
-import {COUNTRIES} from './mixins/WithCountries';
+import * as d3 from "d3";
+import { truncate } from "@js/lib/util";
+import _programme_areas from "@js/constants/programme-areas.json5";
+import { COUNTRIES } from "./mixins/WithCountries";
 
-import WithFiltersMixin from './mixins/WithFilters'
+import WithFiltersMixin from "./mixins/WithFilters";
 
 
 export default {
   mixins: [
-    WithFiltersMixin,
+    WithFiltersMixin
   ],
 
   beforeCreate() {
-    this.format_pa = function(programme_area) {
-      return _programme_areas[programme_area]['short_name'];
+    this.format_pa = function (programme_area) {
+      return _programme_areas[programme_area]["short_name"];
     };
-    this.get_country = function(country_code) {
-      return COUNTRIES[country_code]['name'];
-    }
+    this.get_country = function (country_code) {
+      return COUNTRIES[country_code]["name"];
+    };
     this.FILTER_SETTINGS = {
-      fm: {name:'FM'},
-      beneficiary: {name: 'BS', formatter: this.get_country},
-      region: {name: 'Region'},
-      sector: {name: 'PS', truncate: 20},
-      area: {name:'PA', formatter: this.format_pa},
-      donor: {name: 'DS', formatter: this.get_country},
-      DPP: {name: 'Programme partner', truncate: 60},
-      dpp: {name: 'Project partner', truncate: 60},
-    }
+      fm: { name: "FM" },
+      beneficiary: { name: "BS", formatter: this.get_country },
+      region: { name: "Region" },
+      sector: { name: "PS", truncate: 20 },
+      area: { name: "PA", formatter: this.format_pa },
+      donor: { name: "DS", formatter: this.get_country },
+      DPP: { name: "Programme partner", truncate: 60 },
+      dpp: { name: "Project partner", truncate: 60 }
+    };
   },
 
   computed: {
     data() {
-      const filters = {}
+      const filters = {};
       for (const key in this.filters) {
         if (this.filters[key]) {
           const settings = this.FILTER_SETTINGS[key];
           let filter_value = this.filters[key];
-          if (settings['formatter']) {
-            filter_value = settings['formatter'](filter_value);
+          if (settings["formatter"]) {
+            filter_value = settings["formatter"](filter_value);
           }
-          if (settings['truncate']) {
-            filter_value = truncate(filter_value, settings['truncate']);
+          if (settings["truncate"]) {
+            filter_value = truncate(filter_value, settings["truncate"]);
           }
           filters[key] = {
-            name: settings['name'],
-            value: filter_value,
-          }
+            name: settings["name"],
+            value: filter_value
+          };
         }
       }
 
       return filters;
-    },
+    }
   },
 
   created() {
@@ -203,8 +201,8 @@ export default {
 
   methods: {
     initFiltersStack() {
-      for(const type in this.filters) {
-        if(this.filters[type]) {
+      for (const type in this.filters) {
+        if (this.filters[type]) {
           this.filters_stack.push(type);
         }
       }
@@ -213,8 +211,8 @@ export default {
     handleEsc() {
       const self = this;
       window.addEventListener("keyup", e => {
-          if(e.keyCode == 27) self.removeLastFilter();
-        });
+        if (e.keyCode == 27) self.removeLastFilter();
+      });
     },
 
     hasFilters() {
@@ -232,15 +230,15 @@ export default {
 
     // filters_stack holds the active filters ordered chronologically
     removeLastFilter() {
-      if(this.filters_stack.length > 0) {
-        const lastFilter = this.filters_stack[this.filters_stack.length-1];
+      if (this.filters_stack.length > 0) {
+        const lastFilter = this.filters_stack[this.filters_stack.length - 1];
         this.filters[lastFilter] = null;
       }
     },
 
     resetFilters() {
       for (const filter in this.filters) {
-        this.filters[filter] = null
+        this.filters[filter] = null;
       }
     },
 
@@ -252,7 +250,7 @@ export default {
       const index = this.filters_stack.indexOf(type);
       if (index !== -1) this.filters_stack.splice(index, 1);
       if (val) this.filters_stack.push(type);
-    },
-  },
-}
+    }
+  }
+};
 </script>
