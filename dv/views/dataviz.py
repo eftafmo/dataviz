a124109531e6f2d2967317cf2d3151392f3cffb8
Alexandru Chirila <alex@alexkiro.me>
2021-07-20 10:34:48 +0300

    Rename menu and add placeholders

--- dv/views/dataviz.py
+++ dv/views/dataviz.py
@@ -6,19 +6,19 @@ from django.urls import reverse
 ALLOCATION_PERIODS = {
     "2014-2021": [
         'overview',
-        'grants',
-        'partners',
+        'funding',
+        'cooperation',
         'projects',
-        'sdgs',
+        'global_goals',
     ],
     "2009-2014": [
         'overview',
-        'grants',
-        'partners',
+        'funding',
+        'cooperation',
         'projects',
     ],
     "compare": [
-        'beneficiary',
+        'beneficiary_states',
         'sectors',
     ]
     #"2004-2009": [
@@ -35,7 +35,7 @@ def get_menu():
             "scenarios": [
                 {
                     "id": scenario,
-                    "name": scenario.title(),
+                    "name": scenario.title().replace("_", " "),
                     "url": (
                         reverse('frontend:period', args=[period])
                         if scenario == 'overview' else
@@ -57,7 +57,9 @@ def render(request, period, scenario=None):
     for the given allocation period and scenario.
     """
 
-    if scenario is None:
+    if scenario is None and period == "compare":
+        scenario = 'beneficiary_states'
+    elif scenario is None:
         scenario = 'overview'
 
     if scenario not in ALLOCATION_PERIODS[period]:
