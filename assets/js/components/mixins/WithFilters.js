10be9b612d3526e6d5a1da7388b7bba0644ba0a0
Alexandru Chirila <alex@alexkiro.me>
2021-07-20 10:55:18 +0300

    Fix scenarios

--- assets/js/components/mixins/WithFilters.js
+++ assets/js/components/mixins/WithFilters.js
@@ -7,41 +7,39 @@ export const FILTERS = {
   area: null,
   donor: null,
   DPP: null,
-}
-
+};
 
 // and their usage across scenarios
 export const SCENARIOFILTERS = {
-  index: ['fm', 'beneficiary'],
-  grants: ['fm', 'beneficiary', 'sector', 'area'],
-  partners: ['fm', 'beneficiary', 'sector', 'area', 'donor', 'DPP'],
-  projects: ['fm', 'beneficiary', 'region', 'sector', 'area'],
-  search: ['fm', 'beneficiary', 'sector', 'area', 'donor', 'DPP'],
-}
-
+  index: ["fm", "beneficiary"],
+  funding: ["fm", "beneficiary", "sector", "area"],
+  cooperation: ["fm", "beneficiary", "sector", "area", "donor", "DPP"],
+  projects: ["fm", "beneficiary", "region", "sector", "area"],
+  search: ["fm", "beneficiary", "sector", "area", "donor", "DPP"],
+};
 
 export default {
   data() {
     return {
       filters: FILTERS,
-    }
+    };
   },
 
   watch: {
     // make sure every key exists from the start
-    'filters.fm': 'handleFilterFm',
-    'filters.beneficiary': 'handleFilterBeneficiary',
-    'filters.region': 'handleFilterRegion',
-    'filters.sector': 'handleFilterSector',
-    'filters.area': 'handleFilterArea',
-    'filters.donor': 'handleFilterDonor',
-    'filters.DPP': 'handleFilterDPP',
+    "filters.fm": "handleFilterFm",
+    "filters.beneficiary": "handleFilterBeneficiary",
+    "filters.region": "handleFilterRegion",
+    "filters.sector": "handleFilterSector",
+    "filters.area": "handleFilterArea",
+    "filters.donor": "handleFilterDonor",
+    "filters.DPP": "handleFilterDPP",
   },
 
   methods: {
     handleFilter(type, val, old) {
       // this should be handled by each component specifically
-      return
+      return;
       //throw "Unhandled filter: " + type;
       //console.log(`» [${type}] filter:`, old,'→', val);
     },
@@ -54,8 +52,8 @@ export default {
       this.handleFilter(type, val, old);
     },
     handleFilterRegion(val, old) {
-      const type = "region"
-      this.handleFilter(type, val, old)
+      const type = "region";
+      this.handleFilter(type, val, old);
     },
     handleFilterSector(val, old) {
       const type = "sector";
@@ -74,4 +72,4 @@ export default {
       this.handleFilter(type, val, old);
     },
   },
-}
+};
