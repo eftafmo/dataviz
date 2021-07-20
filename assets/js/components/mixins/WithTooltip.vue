6be4d83362edbef3511fe999a0b9c9674533037c
Alexandru Chirila <alex@alexkiro.me>
2021-07-20 13:22:04 +0300

    Fix tooltip opacity.

--- assets/js/components/mixins/WithTooltip.vue
+++ assets/js/components/mixins/WithTooltip.vue
@@ -1,51 +1,64 @@
+<script>
+export default {
+  mounted() {
+    this.createTooltip();
+  },
+
+  methods: {
+    createTooltip() {
+      throw "Not Implemented";
+    },
+  },
+};
+</script>
+
 <style lang="less">
 @import "@css/style";
 
 .dataviz-tooltip {
-  font-family: Arial;
- li {
-  list-style-type: none;
- }
+  li {
+    list-style-type: none;
+  }
 
- ul {
-  padding-left: 0;
- }
+  ul {
+    padding-left: 0;
+  }
   z-index: 2;
   min-width: 320px;
   max-width: 420px;
   padding: 1rem 2rem;
-  background-color: fadeout(@bg_color, 85);
-  box-shadow: 0px 0px 3px @shadow_color;
+  background-color: fadeout(@bg_color, 10);
+  box-shadow: 0 0 3px @shadow_color;
   .btn {
     position: absolute;
     top: 10px;
     right: 15px;
-    @media(min-width: 768px){
+    @media (min-width: 768px) {
       display: none;
     }
   }
-  @media(max-width:768px) {
+  @media (max-width: 768px) {
     display: none;
-    left: 0!important;
-    right: 0!important;
+    left: 0 !important;
+    right: 0 !important;
     border: 1px solid #3d90f3;
   }
 
   &:after {
-    content: '▼';
+    content: "▼";
     position: absolute;
     left: 45%;
     bottom: 20px;
     width: 0;
     height: 0;
     color: white;
-    text-shadow: 0px 2px 3px @shadow_color;
+    text-shadow: 0 2px 3px @shadow_color;
     font-size: 2em;
     clear: both;
     pointer-events: none;
-     @media(max-width:768px) {
+    @media (max-width: 768px) {
       display: none;
-  }
+    }
   }
 
   .action {
@@ -57,75 +70,43 @@
     text-align: right;
 
     &:before {
-      content:'- ';
+      content: "- ";
     }
   }
 
   .title-container {
-    -js-display: flex;
     display: flex;
     justify-content: flex-start;
     align-items: center;
     padding-bottom: 1rem;
     margin-bottom: 1rem;
-    margin-top: .5rem;
+    margin-top: 0.5rem;
     font-weight: 600;
-    margin-left: .5rem;
-    margin-right: .5rem;
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
     border-bottom: 1px solid #eee;
     &.donor-tooltip {
       border-bottom: none;
     }
     svg {
       max-width: 30px;
-      box-shadow: 0px 0px 2px #aaa;
+      box-shadow: 0 0 2px #aaa;
       height: 20px;
     }
 
     span {
-          white-space: normal;
+      white-space: normal;
     }
   }
 
   .spacing {
-    margin: .6rem 0;
+    margin: 0.6rem 0;
     display: block;
   }
 
-  .name{
-    margin: 0 .5rem;
+  .name {
+    margin: 0 0.5rem;
     font-size: 1.8rem;
   }
 }
-
-
-body.dark .dataviz-tooltip {
-  background-color: fadeout(@dark_bg_color, 85);
-  box-shadow: 0px 0px 3px @dark_shadow_color;
-
-  &:after {
-    text-shadow: 0px 2px 3px @dark_shadow_color;
-  }
-}
 </style>
-
-<script>
-//import * as d3 from 'd3';
-//import d3tip from 'd3-tip';
-
-
-export default {
-
-mounted() {
-    this.createTooltip();
-},
-
-methods: {
-    createTooltip() {
-      throw "Not Implemented"
-    },
-},
-
-}
-
-</script>
