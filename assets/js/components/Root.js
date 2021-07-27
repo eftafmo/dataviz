5a1d0b333ed43f9938f665ec0d8a3a9779a8d966
Alexandru Chirila <alex@alexkiro.me>
2021-07-27 12:27:43 +0300

    Fix titles

--- assets/js/components/Root.js
+++ assets/js/components/Root.js
@@ -1,12 +1,9 @@
-import debounce from 'lodash.debounce';
+import debounce from "lodash.debounce";
 
-import Base from './Base';
-
-import Embeddor from './includes/Embeddor'
+import Base from "./Base";
 
 // access filters directly before they get bound, to avoid triggering handlers
-import {FILTERS, SCENARIOFILTERS} from './mixins/WithFilters'
-
+import { FILTERS, SCENARIOFILTERS } from "./mixins/WithFilters";
 
 function getURL(obj) {
   // obj must have a .href property.
@@ -18,110 +15,60 @@ function getURL(obj) {
   return url;
 }
 
-const _scenario_url = new RegExp(
-  '^/(\\d{4}-\\d{4})/(\\w+)?(/|\.html)?$'
-)
+const _scenario_url = new RegExp("^/(\\d{4}-\\d{4})/(\\w+)?(/|.html)?$");
 
 function getScenario(url) {
-  const match = url.pathname.match(_scenario_url)
-  if (!match) return {}
+  const match = url.pathname.match(_scenario_url);
+  if (!match) return {};
 
-  const period = match[1]
-  let scenario = match[2]
-  if (scenario === undefined) scenario = "index"
+  const period = match[1];
+  let scenario = match[2];
+  if (scenario === undefined) scenario = "index";
   // test if this is a known scenario
-  if (SCENARIOFILTERS[scenario] === undefined) return {}
+  if (SCENARIOFILTERS[scenario] === undefined) return {};
 
-  return {period, scenario}
+  return { period, scenario };
 }
-
 
 export default {
   extends: Base,
-
-  components: {
-    embeddor: Embeddor,
-  },
-
-  /*
-  data() {
-    return {
-      datasource: null,
-      vizcomponents: [],
-    };
-  },
-  */
-
   props: {
     datasource: String,
   },
-
   beforeCreate() {
     // set filters from querystring.
     const url = getURL(window.location),
-          params = url.searchParams,
-          {period, scenario} = getScenario(url)
+      params = url.searchParams,
+      { period, scenario } = getScenario(url);
 
-    const filters = SCENARIOFILTERS[scenario]
+    const filters = SCENARIOFILTERS[scenario];
 
     for (const name of filters) {
-      let param = params.get(name) || null
+      let param = params.get(name) || null;
       if (param) {
-        param = param.replace(/\+/g, ' ')
+        param = param.replace(/\+/g, " ");
       }
-      FILTERS[name] = param
+      FILTERS[name] = param;
     }
 
-    this.scenario = scenario
+    this.scenario = scenario;
   },
-
   created() {
     // don't rely on the backend to serve updated anchors
     this.updateAnchors();
   },
-
-  mounted() {
-    /** !!! WARNING, TODO !!! **/
-    this.vizcomponents = []
-    return;
-
-    /*
-    // TODO: this was used to register the embedding widget
-    // with all child components, and doesn't work anymore.
-    // ($children was removed in vue 3)
-    */
-
-    const vizcomps = []
-    // recurse through all children to find those that are dataviz
-    let parent = this
-
-    const recurse = (current) => {
-      for (const comp of current.$children) {
-        if (comp.$options.isDataviz)
-          vizcomps.push(comp) // end of the road
-        else
-          recurse(comp) // not end of the road
-      }
-    }
-
-    recurse(this)
-
-    this.vizcomponents = vizcomps
-  },
-
-
   methods: {
     updateAnchors() {
       // we can run this async
       let updater = this._debouncedUpdateAnchors;
       if (updater === undefined)
         updater = this._debouncedUpdateAnchors = debounce(
-          this._updateAnchors, 100
+          this._updateAnchors,
+          100
         );
 
       updater();
     },
-
     _updateAnchors() {
       const location = getURL(window.location);
 
@@ -131,15 +78,14 @@ export default {
 
         const url = getURL(a);
         if (url.origin !== location.origin) continue;
-        const {period, scenario} = getScenario(url)
-        if (!scenario) continue
+        const { period, scenario } = getScenario(url);
+        if (!scenario) continue;
 
-        this._updateURL(url, SCENARIOFILTERS[scenario])
+        this._updateURL(url, SCENARIOFILTERS[scenario]);
 
         a.href = url.href;
       }
     },
-
     _updateURL(url, filters) {
       const params = url.searchParams;
 
@@ -149,11 +95,9 @@ export default {
         // remove the param and add it back if necessary
         // (so we have a nice, predictable order. not OCD at all.)
         params.delete(name);
-        if (value)
-          params.set(name, value);
+        if (value) params.set(name, value);
       }
     },
-
     handleFilter(type, val, old) {
       const location = getURL(window.location);
       this._updateURL(location, SCENARIOFILTERS[this.scenario]);
@@ -163,4 +107,4 @@ export default {
       this.updateAnchors();
     },
   },
-}
+};
