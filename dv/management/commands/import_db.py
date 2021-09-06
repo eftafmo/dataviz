699fdff348edac51b75609e50d2cb0e30bb4bf36
Alexandru Chirila <alex@alexkiro.me>
2021-09-06 15:51:40 +0300

    Use new connection for each query.

--- dv/management/commands/import_db.py
+++ dv/management/commands/import_db.py
@@ -1,4 +1,5 @@
 import re
+from contextlib import contextmanager
 
 import bleach
 import pymssql
@@ -24,40 +25,49 @@ def sanitize_html(text):
     return COMMENTS_PATTERN.sub('', cleaned_text)
 
 
+@contextmanager
+def db_cursor():
+    conn = pymssql.connect(
+        settings.MSSQL_SERVER,
+        settings.MSSQL_USERNAME,
+        settings.MSSQL_PASSWORD,
+        settings.MSSQL_DATABASE
+    )
+    cursor = conn.cursor(as_dict=True)
+    try:
+        yield cursor
+    finally:
+        cursor.close()
+        conn.close()
+
+
 class Command(BaseCommand):
     help = 'Import data from grACE db'
 
     def handle(self, *args, **options):
-        conn = pymssql.connect(
-            settings.MSSQL_SERVER,
-            settings.MSSQL_USERNAME,
-            settings.MSSQL_PASSWORD,
-            settings.MSSQL_DATABASE
-        )
-        cursor = conn.cursor(as_dict=True)
-
         states = {state.name: state for state in State.objects.all()}
 
         programme_area_query = 'SELECT * FROM fmo.TR_RDPProgrammeArea'
-        cursor.execute(programme_area_query)
-        programme_areas = {}
-        priority_sectors = {}
-        for row in cursor.fetchall():
-            priority_sector, _ = PrioritySector.objects.get_or_create(
-                code=row['PSCode'],
-                defaults={'name': row['PrioritySector']},
-            )
-            priority_sectors[priority_sector.code] = priority_sector
-            programme_area = ProgrammeArea.objects.create(
-                funding_period=FUNDING_PERIOD,
-                code=row['PACode'],
-                name=row['ProgrammeArea'],
-                short_name=row['ProgrammeAreaShortName'],
-                order=row['idPA'],
-                priority_sector=priority_sector,
-                objective=row['Objective'] or '',
-            )
-            programme_areas[programme_area.code] = programme_area
+        with db_cursor() as cursor:
+            cursor.execute(programme_area_query)
+            programme_areas = {}
+            priority_sectors = {}
+            for row in cursor.fetchall():
+                priority_sector, _ = PrioritySector.objects.get_or_create(
+                    code=row['PSCode'],
+                    defaults={'name': row['PrioritySector']},
+                )
+                priority_sectors[priority_sector.code] = priority_sector
+                programme_area = ProgrammeArea.objects.create(
+                    funding_period=FUNDING_PERIOD,
+                    code=row['PACode'],
+                    name=row['ProgrammeArea'],
+                    short_name=row['ProgrammeAreaShortName'],
+                    order=row['idPA'],
+                    priority_sector=priority_sector,
+                    objective=row['Objective'] or '',
+                )
+                programme_areas[programme_area.code] = programme_area
 
         self.stdout.write(self.style.SUCCESS(
             f'Imported {PrioritySector.objects.count()} PrioritySector objects.'))
@@ -65,45 +75,47 @@ class Command(BaseCommand):
             f'Imported {ProgrammeArea.objects.count()} ProgrammeArea objects.'))
 
         allocation_query = 'SELECT * FROM fmo.TR_RDPCountryProgrammeArea'
-        cursor.execute(allocation_query)
-        for row in cursor.fetchall():
-            Allocation.objects.create(
-                funding_period=FUNDING_PERIOD,
-                financial_mechanism=GRANT_SHORT_NAME_TO_FM[row['GrantShortName']],
-                state=states.get(row['Country']),
-                programme_area=programme_areas.get(row['PACode']),
-                gross_allocation=row['GrossAllocation'],
-                net_allocation=row['NetAllocation'],
-                thematic=row['Thematic'] or '',
-            )
+        with db_cursor() as cursor:
+            cursor.execute(allocation_query)
+            for row in cursor.fetchall():
+                Allocation.objects.create(
+                    funding_period=FUNDING_PERIOD,
+                    financial_mechanism=GRANT_SHORT_NAME_TO_FM[row['GrantShortName']],
+                    state=states.get(row['Country']),
+                    programme_area=programme_areas.get(row['PACode']),
+                    gross_allocation=row['GrossAllocation'],
+                    net_allocation=row['NetAllocation'],
+                    thematic=row['Thematic'] or '',
+                )
 
         self.stdout.write(self.style.SUCCESS(
             f'Imported {Allocation.objects.count()} Allocation objects.'))
 
         programme_query = 'SELECT * FROM fmo.TR_RDPProgramme'
-        cursor.execute(programme_query)
-        programmes = {}
-        for row in cursor.fetchall():
-            programme = Programme.objects.create(
-                funding_period=FUNDING_PERIOD,
-                short_name=row['ProgrammeShortName'],
-                name=row['Programme'],
-                summary=sanitize_html(row['ProgrammeSummary']),
-                status=row['ProgrammeStatus'] or '',
-                allocation_eea=row['ProgrammeGrantEEA'] or 0,
-                allocation_norway=row['ProgrammeGrantNorway'] or 0,
-                co_financing=row['ProgrammeCoFinancing'],
-                is_tap=row['IsTAProgramme'],
-                is_bfp=row['IsBFProgramme'],
-            )
-            programmes[programme.short_name] = programme
+        with db_cursor() as cursor:
+            cursor.execute(programme_query)
+            programmes = {}
+            for row in cursor.fetchall():
+                programme = Programme.objects.create(
+                    funding_period=FUNDING_PERIOD,
+                    short_name=row['ProgrammeShortName'],
+                    name=row['Programme'],
+                    summary=sanitize_html(row['ProgrammeSummary']),
+                    status=row['ProgrammeStatus'] or '',
+                    allocation_eea=row['ProgrammeGrantEEA'] or 0,
+                    allocation_norway=row['ProgrammeGrantNorway'] or 0,
+                    co_financing=row['ProgrammeCoFinancing'],
+                    is_tap=row['IsTAProgramme'],
+                    is_bfp=row['IsBFProgramme'],
+                )
+                programmes[programme.short_name] = programme
 
-            self._add_m2m_entries(programme, row, 'ProgrammeAreaList', 'programme_areas',
-                                  'ProgrammeArea', programme_areas)
+                self._add_m2m_entries(programme, row, 'ProgrammeAreaList', 'programme_areas',
+                                      'ProgrammeArea', programme_areas)
 
-            # For the moment, all programmes have one or zero (NULL/Non-country specific) states
-            self._add_m2m_entries(programme, row, 'Country', 'states',
-                                  'State', states)
+                # For the moment, all programmes have one or zero (NULL/Non-country specific) states
+                self._add_m2m_entries(programme, row, 'Country', 'states',
+                                      'State', states)
 
         self.stdout.write(self.style.SUCCESS(
             f'Imported {Programme.objects.count()} Programme objects.'))
@@ -115,92 +127,96 @@ class Command(BaseCommand):
             'AS RowNum, * '
             'FROM fmo.TR_RDPProject) n WHERE RowNum = 1;'
         )
-        cursor.execute(project_query)
-        projects = {}
-        for row in cursor.fetchall():
-            project = Project.objects.create(
-                funding_period=FUNDING_PERIOD,
-                code=row['ProjectCode'],
-                name=row['Project'],
-                status=row['ProjectContractStatus'],
-                state=states.get(row['Country']),
-                programme=programmes.get(row['ProgrammeShortName']),
-                nuts_code=row['ProjectLocation'] or '',
-                url=row['ProjectURL'],
-                allocation=row['ProjectGrant'],
-                is_eea=bool(row['IdFinancialMechanismEEA']),
-                is_norway=bool(row['IdFinancialMechanismNorway']),
-                has_ended=row['Hasended'],
-                is_dpp=row['isdpp'],
-                is_positive_fx=bool(row['ResultPositiveEffects']),
-                is_improved_knowledge=bool(row['ResultsImprovedKnowledge']),
-                is_continued_coop=bool(row['CooperationContinue']),
-                initial_description=sanitize_html(row['ProjectInitialDescriptionHtml']),
-                results_description=sanitize_html(row['ProjectResultsDescriptionHtml']),
-            )
-            projects[project.code] = project
+        with db_cursor() as cursor:
+            cursor.execute(project_query)
+            projects = {}
+            for row in cursor.fetchall():
+                project = Project.objects.create(
+                    funding_period=FUNDING_PERIOD,
+                    code=row['ProjectCode'],
+                    name=row['Project'],
+                    status=row['ProjectContractStatus'],
+                    state=states.get(row['Country']),
+                    programme=programmes.get(row['ProgrammeShortName']),
+                    nuts_code=row['ProjectLocation'] or '',
+                    url=row['ProjectURL'],
+                    allocation=row['ProjectGrant'],
+                    is_eea=bool(row['IdFinancialMechanismEEA']),
+                    is_norway=bool(row['IdFinancialMechanismNorway']),
+                    has_ended=row['Hasended'],
+                    is_dpp=row['isdpp'],
+                    is_positive_fx=bool(row['ResultPositiveEffects']),
+                    is_improved_knowledge=bool(row['ResultsImprovedKnowledge']),
+                    is_continued_coop=bool(row['CooperationContinue']),
+                    initial_description=sanitize_html(row['ProjectInitialDescriptionHtml']),
+                    results_description=sanitize_html(row['ProjectResultsDescriptionHtml']),
+                )
+                projects[project.code] = project
 
-            self._add_m2m_entries(project, row, 'ProgrammeAreaCodesList', 'programme_areas',
-                                  'ProgrammeArea', programme_areas)
-            self._add_m2m_entries(project, row, 'PrioritySectorCodesList', 'priority_sectors',
-                                  'PrioritySector', priority_sectors)
+                self._add_m2m_entries(project, row, 'ProgrammeAreaCodesList', 'programme_areas',
+                                      'ProgrammeArea', programme_areas)
+                self._add_m2m_entries(project, row, 'PrioritySectorCodesList', 'priority_sectors',
+                                      'PrioritySector', priority_sectors)
 
         self.stdout.write(self.style.SUCCESS(
             f'Imported {Project.objects.count()} Project objects.'))
 
         indicator_query = 'SELECT * FROM fmo.TR_RDPIndicators'
-        cursor.execute(indicator_query)
-        for row in cursor.fetchall():
-            Indicator.objects.create(
-                funding_period=FUNDING_PERIOD,
-                programme=programmes.get(row['ProgrammeShortName']),
-                programme_area=programme_areas.get(row['PACode']),
-                state=states.get(row['Country']),
-                indicator=row['CoreCommonIndicator'],
-                outcome=row['Outcome'],
-                header=row['Header'],
-                unit_of_measurement=row['UnitOfMeasurement'],
-                achievement_eea=row['Achievement_EEA'] or 0,
-                achievement_norway=row['Achievement_Norway'] or 0,
-                is_core=bool(row['IsCore']),
-                is_common=bool(row['IsCommon']),
-            )
+        with db_cursor() as cursor:
+            cursor.execute(indicator_query)
+            for row in cursor.fetchall():
+                Indicator.objects.create(
+                    funding_period=FUNDING_PERIOD,
+                    programme=programmes.get(row['ProgrammeShortName']),
+                    programme_area=programme_areas.get(row['PACode']),
+                    state=states.get(row['Country']),
+                    indicator=row['CoreCommonIndicator'],
+                    outcome=row['Outcome'],
+                    header=row['Header'],
+                    unit_of_measurement=row['UnitOfMeasurement'],
+                    achievement_eea=row['Achievement_EEA'] or 0,
+                    achievement_norway=row['Achievement_Norway'] or 0,
+                    is_core=bool(row['IsCore']),
+                    is_common=bool(row['IsCommon']),
+                )
 
         self.stdout.write(self.style.SUCCESS(
             f'Imported {Indicator.objects.count()} Indicator objects.'))
 
         organisation_role_query = 'SELECT * FROM fmo.TR_RDPOrganisationRole'
-        cursor.execute(organisation_role_query)
-        for row in cursor.fetchall():
-            OrganisationRole.objects.create(
-                funding_period=FUNDING_PERIOD,
-                organisation_country=row['CountryOrganisation'],
-                organisation_name=row['Organisation'],
-                nuts_code=row['NUTSCode'] or '',
-                role_code=row['OrganisationRoleCode'],
-                role_name=row['OrganisationRole'],
-                programme=programmes.get(row['ProgrammeCode']),
-                project=projects.get(row['ProjectCode']),
-                state=states.get(row['CountryRole']),
-            )
+        with db_cursor() as cursor:
+            cursor.execute(organisation_role_query)
+            for row in cursor.fetchall():
+                OrganisationRole.objects.create(
+                    funding_period=FUNDING_PERIOD,
+                    organisation_country=row['CountryOrganisation'],
+                    organisation_name=row['Organisation'],
+                    nuts_code=row['NUTSCode'] or '',
+                    role_code=row['OrganisationRoleCode'],
+                    role_name=row['OrganisationRole'],
+                    programme=programmes.get(row['ProgrammeCode']),
+                    project=projects.get(row['ProjectCode']),
+                    state=states.get(row['CountryRole']),
+                )
         self.stdout.write(self.style.SUCCESS(
             f'Imported {OrganisationRole.objects.count()} OrganisationRole objects.'))
 
         bilateral_initiative_query = 'SELECT * FROM fmo.TR_RDPBilateralinitiative'
-        cursor.execute(bilateral_initiative_query)
-        for row in cursor.fetchall():
-            bilateral_initiative = BilateralInitiative.objects.create(
-                funding_period=FUNDING_PERIOD,
-                code=row['BICode'],
-                title=row['BITitle'],
-                programme=programmes.get(row['ProgrammeShortName']),
-                project=projects.get(row['ProjectCode']),
-                state=states.get(row['Country']),
-                level=row['Level'] or '',
-                status=row['BIStatus'],
-            )
-            self._add_m2m_entries(bilateral_initiative, row, 'ProgrammeAreaCodesList', 'programme_areas',
-                                  'ProgrammeArea', programme_areas)
+        with db_cursor() as cursor:
+            cursor.execute(bilateral_initiative_query)
+            for row in cursor.fetchall():
+                bilateral_initiative = BilateralInitiative.objects.create(
+                    funding_period=FUNDING_PERIOD,
+                    code=row['BICode'],
+                    title=row['BITitle'],
+                    programme=programmes.get(row['ProgrammeShortName']),
+                    project=projects.get(row['ProjectCode']),
+                    state=states.get(row['Country']),
+                    level=row['Level'] or '',
+                    status=row['BIStatus'],
+                )
+                self._add_m2m_entries(bilateral_initiative, row, 'ProgrammeAreaCodesList', 'programme_areas',
+                                      'ProgrammeArea', programme_areas)
 
         self.stdout.write(self.style.SUCCESS(
             f'Imported {BilateralInitiative.objects.count()} BilateralInitiative objects.'))
