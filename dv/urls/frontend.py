df14ae3f46dc7c2646829e67f6e4ab7dc41410f9
Alexandru Chirila <alex@alexkiro.me>
2021-10-06 09:44:08 +0300

    Fix export link.

--- dv/urls/frontend.py
+++ dv/urls/frontend.py
@@ -28,14 +28,14 @@ urlpatterns = [
     url(r"^disclaimer/$", views.disclaimer, name="disclaimer"),
     url(r"^search/$", views.ProgrammeFacetedSearchView.as_view(), name="search"),
     url(
-        r"^search/bilateralinitiatives/$",
+        r"^search/bilateralinitiative/$",
         views.BilateralInitiativesSearchView.as_view(),
-        name="search_bilateralinitiatives",
+        name="search_bilateralinitiative",
     ),
     url(
-        r"^search/bilateralinitiatives/export/$",
+        r"^search/bilateralinitiative/export/$",
         views.BilateralInitiativeFacetedExportView.as_view(),
-        name="bilateralinitiatives_export",
+        name="bilateralinitiative_export",
     ),
     url(
         r"^search/programme/$",
