269035b8522a81839ada547a1ab6cce1ad9eeea9
ValentinConstantin <valentinmconstantin@gmail.com>
2021-10-26 15:32:28 +0300

    Replace 'programmes' with 'programmes and agreements' in tooltips

--- assets/js/components/GlobalGoalsBarChart.vue
+++ assets/js/components/GlobalGoalsBarChart.vue
@@ -31,6 +31,52 @@ export default {
       ],
     };
   },
+  methods: {
+    tooltipTemplate(ev, d) {
+      let programmeSize = `
+          <li>
+            ${d.programmes.size}
+            ${this.singularize("programmes", d.programmes.size)}
+          </li> `;
+      if (d.sdg_no === 16 || d.sdg_no === 17) {
+        programmeSize = `
+            <li>${d.programmes.size}
+            ${
+              d.programmes.size === 1
+                ? "programme/agreement"
+                : "programmes and agreements"
+            }
+            </li>`;
+      }
+      return `
+        <div class="title-container">
+          <span>${d.name}</span>
+        </div>
+        <ul>
+          <li>
+            ${this.currency(d.allocation)}
+            ${this.allocationType} allocation
+          </li>
+          <li>
+            ${this.getBeneficiaryCount(d.beneficiaries)}
+            ${this.singularize(
+              "Beneficiary States",
+              this.getBeneficiaryCount(d.beneficiaries)
+            )}
+          </li>
+          <li>
+            ${d.sectors.size}
+            ${this.singularize("sectors", d.sectors.size)}
+          </li>
+          <li>
+            ${d.areas.size}
+            ${this.singularize("programme areas", d.areas.size)}
+          </li>
+          ${programmeSize}
+        </ul>
+      `;
+    },
+  },
 };
 </script>
 
