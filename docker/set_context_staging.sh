0d4f719b1fc87cfd363fca745adcd7f05819cd79
Andrei Melis <andrei.melis@eaudeweb.ro>
2021-11-02 20:59:15 +0200

    Cleanup azure and added .prod.example files, refs #11397

--- docker/set_context_staging.sh
+++
@@ -1,8 +0,0 @@
-EEAG_REGION=northeurope
-EEAG_RESOURCE_GROUP=eeagstaging
-EEAG_VAULT=eeagsecrets
-EEAG_STORAGE_ACCOUNT=eeagstorage
-
-docker context list -q | grep -q $EEAG_RESOURCE_GROUP || docker context create aci $EEAG_RESOURCE_GROUP
-export DOCKER_CONTEXT=$EEAG_RESOURCE_GROUP
-PS1="[EEA Grants staging]$PS1"
