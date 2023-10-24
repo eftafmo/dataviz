a35f7cb16b1d5a9b84677fde3356543c0b82acca
Alexandru Chirila <1538458+alexkiro@users.noreply.github.com>
2023-10-24 09:33:22 +0300

    Add django-debug-toolbar

--- dv/urls/__init__.py
+++ dv/urls/__init__.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
 
 from . import api, frontend
 
@@ -14,3 +15,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
