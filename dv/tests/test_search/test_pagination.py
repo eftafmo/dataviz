da87fab258f235b485a7be03b4980df90b3ba708
andreighh <61791875+andreighh@users.noreply.github.com>
2026-09-24 16:43:50 +0300

    Mock elastic search

--- dv/tests/test_search/test_pagination.py
+++ dv/tests/test_search/test_pagination.py
@@ -1,25 +1,35 @@
-from django.test import RequestFactory, TestCase
+from collections import defaultdict
+from unittest.mock import patch
+
+from django.test import TestCase
+from django.urls import reverse
 
 from dv.views.frontend import ProgrammeFacetedSearchView
 
 
-class TestPaginateBy(TestCase):
-    def paginate_by(self, value):
-        view = ProgrammeFacetedSearchView()
-        view.setup(RequestFactory().get("/search/programme/", {"paginate_by": value}))
-        return view.get_paginate_by(None)
+# Don't depend on a frontend build or a running Elasticsearch
+@patch("dv.context.load_manifest", return_value=defaultdict(str))
+@patch(
+    "dv.lib.es7.CustomES7SearchBackend.search",
+    return_value={"results": [], "hits": 0, "facets": {"fields": {}}},
+)
+class TestSearchPaginateBy(TestCase):
+    url = reverse("frontend:search")
 
-    def test_allowed_sizes(self):
+    def get_per_page(self, **params):
+        resp = self.client.get(self.url, params)
+        self.assertEqual(resp.status_code, 200)
+        return resp.context["paginator"].per_page
+
+    def test_allowed_sizes(self, *_mocks):
         for size in ProgrammeFacetedSearchView.page_sizes:
             with self.subTest(size=size):
-                self.assertEqual(self.paginate_by(str(size)), size)
+                self.assertEqual(self.get_per_page(paginate_by=size), size)
 
-    def test_invalid_values_fall_back_to_default(self):
+    def test_invalid_values_fall_back_to_default(self, *_mocks):
         for value in ["abc", "", "0", "-10", "7", "100000", "10.5", "1e3"]:
             with self.subTest(value=value):
-                self.assertEqual(self.paginate_by(value), 10)
+                self.assertEqual(self.get_per_page(paginate_by=value), 10)
 
-    def test_missing_uses_default(self):
-        view = ProgrammeFacetedSearchView()
-        view.setup(RequestFactory().get("/search/programme/"))
-        self.assertEqual(view.get_paginate_by(None), 10)
+    def test_missing_uses_default(self, *_mocks):
+        self.assertEqual(self.get_per_page(), 10)
